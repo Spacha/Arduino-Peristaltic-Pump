e2deb62d1525e92e207a691970c8ab68c67b2774
Miika Sikala <miikasikala96@gmail.com>
2019-04-24 03:22:20 +0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -7,50 +7,93 @@
 
 #include "main.h"
 
-// Motor
+// Constants
 const int stepsPerRev = 200;  // this is motor specific
-const int potPin = A0;
+const int potPin = A0;        // for debug
 
 // Construct a stepper instance
 Stepper pump(stepsPerRev, 3, 4);
 
-bool dispense = false;
-int potVal = 0;
+// Create button: NexButton(page_id, obj_id, obj_name)
+NexButton dispenseBtn = NexButton(0, 2, "btn_dispense");
 
+bool dispense = false;        // wheter we are operating the pump or not
+int potVal = 0;               // defines the engine speed
+
+NexTouch *nex_listen_list[] = 
+{
+    &dispenseBtn,
+    NULL
+};
 
 
 void setup()
 {
-  // Serial.begin(9600);
+  nexInit();
 
-  // debug
-  dispense = true;
+  // attach callbacks for events
+  dispenseBtn.attachPop(dispenseBtnPopped);
+  dispenseBtn.attachPush(dispenseBtnPushed);
+
+  pinMode(5, OUTPUT);
 }
 
 
 void loop()
 {
+  nexLoop(nex_listen_list);
   potVal = analogRead(potPin);
+
+  digitalWrite(5, dispense);
 
   // let it flow!
   if (dispense)
   {
-    // we must map the pot val to a reasonable range
+    // map the pot val to a reasonable range
     operatePump(map(potVal, 0, 1023, 0, 100));
+  } else {
+    operatePump(0);
   }
 }
 
+
+
+/***********************************
+ *          FUNCTIONS
+ **********************************/
+
 /**
- * Operates the pump at predefined speed.
+ * Callback function for dispense button.
+ * Called when button is pressed.
+ * 
+ * @return void
+ */
+void dispenseBtnPushed()
+{
+    dispense = true;
+}
+
+/**
+ * Callback function for dispense button.
+ * Called when button is released.
+ * 
+ * @return void
+ */
+void dispenseBtnPopped()
+{
+    dispense = false;
+}
+
+
+/**
+ * Operates the pump at given speed.
  * 
  * @param int speed
  * @return void
  */
 void operatePump(int speed)
 {
-  if (speed > 0)
-  {
-    pump.setSpeed(speed);
-    pump.step(speed / 100);
-  }
+  // if (speed > 0)
+  pump.setSpeed(speed);
+  pump.step(speed / 100);
 }
